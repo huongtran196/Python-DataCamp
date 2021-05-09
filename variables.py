3d98ec55b88c95b8c489281ccac0947bba951a90
Huong Tran <huong.tran0196@gmail.com>
2021-05-09 23:00:22 +0300

    Create Variables

---
+++ variables.py
@@ -0,0 +1,14 @@
+#Importing Python modules
+import statsmodels as sm
+import seaborn as sns
+import numpy as np
+
+#Creating variables
+bayes_age = 4.0
+print(bayes_age)
+favorite_toy = 'Mr.Squeaky'
+owner = 'DataCamp'
+print(favorite_toy)
+print(owner)
+birthday = '2017-07-14'
+case_id = 'DATACAMP!123-456?'
