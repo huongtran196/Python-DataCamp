7055444e9a85e2a6a812f673ba8b709c5096a86c
Huong Tran <huong.tran0196@gmail.com>
2021-05-11 21:44:54 +0300

    Add title, label and floating text

--- Plotting Data with Matplotlib/line_plot.py
+++ Plotting Data with Matplotlib/line_plot.py
@@ -1,5 +1,13 @@
 import matplotlib.pyplot as plt
-plt.plot(deshaun.day_of_week, deshaun.hours_worked)
-plt.plot(aditya.day_of_week, aditya.hours_worked)
-plt.plot(mengfei.day_of_week, mengfei.hours_worked)
+
+plt.plot(deshaun.day_of_week, deshaun.hours_worked, label='Deshaun')
+plt.plot(aditya.day_of_week, aditya.hours_worked, label='Aditya')
+plt.plot(mengfei.day_of_week, mengfei.hours_worked, label='Mengfei')
+
+plt.title('Officer Work Hours')
+plt.xlabel('Day of Week')
+plt.ylabel('Hours Worked')
+plt.text(2.5, 80, 'Missing June data')
+
+plt.legend()
 plt.show()
