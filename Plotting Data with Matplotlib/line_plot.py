d9cd6729e3ac4b6a40f7b7904fb327306dfebe69
Huong Tran <huong.tran0196@gmail.com>
2021-05-11 21:25:57 +0300

    Create line plot

---
+++ Plotting Data with Matplotlib/line_plot.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+plt.plot(deshaun.day_of_week, deshaun.hours_worked)
+plt.plot(aditya.day_of_week, aditya.hours_worked)
+plt.plot(mengfei.day_of_week, mengfei.hours_worked)
+plt.show()
