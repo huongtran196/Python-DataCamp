630c47c7ad2247a95ed9853f27656833374d38e4
Huong Tran <huong.tran0196@gmail.com>
2021-05-13 22:15:39 +0300

    Create List

---
+++ Introduction to Python/list.py
@@ -0,0 +1,21 @@
+# Create a list
+# area variables
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# Create a list areas
+areas = [hall, kit, liv, bed, bath]
+print(areas)
+
+# Update the list
+house = [['hallway', hall], 
+        ['kitchen', kit], 
+        ['living room', liv], 
+        ['bedroom', bed], 
+        ['bathroom', bath]]
+print(house)
+print(type(house))
+
