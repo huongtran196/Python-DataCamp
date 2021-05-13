c60d2839179ff1e751e3788e5d1987355e7ce7ae
Huong Tran <huong.tran0196@gmail.com>
2021-05-13 21:56:40 +0300

    Calculation with variables & Converting types

--- Introduction to Python/variables.py
+++ Introduction to Python/variables.py
@@ -12,3 +12,24 @@ print(favorite_toy)
 print(owner)
 birthday = '2017-07-14'
 case_id = 'DATACAMP!123-456?'
+
+# Calculation with variables
+savings = 100
+growth_multiplier = 1.1
+result = 100 * 1.1 ** 7
+print(result)
+desc = 'compound interest'
+profitable = True 
+
+# Variable Types & Converting types
+year1 = savings * growth_multiplier
+print(type(year1))
+doubledesc = desc + desc
+print(doubledesc)
+
+print('I started with $' + str(savings) + 'and now have $' + str(result) + '. Awesome!')
+pi_string = '3.1415'
+pi.float = float(pi_string)
+
+
+
