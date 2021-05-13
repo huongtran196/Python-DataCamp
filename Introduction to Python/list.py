2fdfce60f7c4165e6676cffbb263aea7a1dd551c
Huong Tran <huong.tran0196@gmail.com>
2021-05-13 22:28:24 +0300

    Subsetting list

--- Introduction to Python/list.py
+++ Introduction to Python/list.py
@@ -19,3 +19,20 @@ house = [['hallway', hall],
 print(house)
 print(type(house))
 
+# Subset & conquer
+areas = ['hallway', 11.25,
+        'kitchen', 18.0,
+        'living room', 20.0,
+        'bedroom', '10.75',
+        'bathroom', 9.50]
+
+# Print out the second element from areas
+print(areas[1])
+
+# Print out the last element from areas
+print(areas[9])
+
+# Print out the area of living room
+print(areas[4:6])
+
+
