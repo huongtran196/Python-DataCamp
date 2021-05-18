5f2fcb16c07ca506c90c349d4b911b25396a952d
Huong Tran <huong.tran0196@gmail.com>
2021-05-18 21:44:53 +0300

    List Method

--- Introduction to Python/function_package.py
+++ Introduction to Python/function_package.py
@@ -33,4 +33,10 @@ areas = [11.25, 18.0, 20.0, 10.75, 9.50]
 print(areas.index(20.0)) # Print out the position of the element 20.0
 print(areas.count(9.50)) # Print out how often 9.50 appear in the list
 
+# List Method: append() & reverse() & remove()
+areas.append(24.5)
+areas.append(15.45)
+areas.reverse()
+print(areas)
+
 
