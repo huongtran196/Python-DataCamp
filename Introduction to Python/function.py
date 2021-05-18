4bb96a95ae1cb240be98e804fe8758f55f1bfaf3
Huong Tran <huong.tran0196@gmail.com>
2021-05-18 17:34:29 +0300

    Functions in Python

---
+++ Introduction to Python/function.py
@@ -0,0 +1,20 @@
+# Create variable var1 and var2
+var1 = [1, 2, 3, 4]
+var2 = True
+
+# Print out the type of var1
+print(type(var1))
+
+# Print out the length of var1
+print(len(var1))
+
+# Convert var2 into interger: out2
+out2 = int(var2)
+
+# Create 2 lists
+first = [11.25, 18.0, 20.0]
+second = [10.75, 9.50]
+
+full = first + second
+full_sorted = sorted(full, reverse= True) # Sort full list in descending order
+print(full_sorted)
