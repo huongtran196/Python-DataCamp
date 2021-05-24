13814f372f9e131a6a13fcda5ef50b4f10a3b14d
Huong Tran <huong.tran0196@gmail.com>
2021-05-24 23:38:35 +0300

    Subsetting Numpy array

--- Introduction to Python/numpy.py
+++ Introduction to Python/numpy.py
@@ -52,3 +52,13 @@ print(light)
 
 # Print out BMIs of all baseball players whose BMI is below 21
 print(bmi[light])
+
+# Store weight and height lists as numpy arrays
+np_weight_lb = np.array(weight_lb)
+np_height_in = np.array(height_in)
+
+# Print out the weight at index 50
+print(np_weight_lb[50])
+
+# Print out sub-array of np_height_in: index 100 up to and including index 110
+print(np_height_in[100:111])
