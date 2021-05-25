338e565c9e7cb795c56893d3561e16cfdfb53ecb
Huong Tran <huong.tran0196@gmail.com>
2021-05-25 14:38:27 +0300

    Dictionary Manipulation

--- Loading Data in Pandas /dictionary.py
+++ Loading Data in Pandas /dictionary.py
@@ -13,3 +13,37 @@ europe = {'spain': 'madrid', 'france': 'paris', 'germany': 'berlin', 'norway': '
 print(europe)
 print(europe.key())
 print(europe['norway'])
+
+# Add italy to europe
+europe['italy'] = 'rome'
+print('italy' in europe)
+# Add poland to europe
+europe['poland'] = 'warsaw'
+print(europe)
+
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'bonn',
+          'norway':'oslo', 'italy':'rome', 'poland':'warsaw',
+          'australia':'vienna' }
+
+# Update capital of Germany
+europe['germany'] = 'berlin'
+# Remove Australia
+del(europe['autralia'])
+print(europe)
+
+# Dictionary of dictionaries
+europe = { 'spain': { 'capital':'madrid', 'population':46.77 },
+           'france': { 'capital':'paris', 'population':66.03 },
+           'germany': { 'capital':'berlin', 'population':80.62 },
+           'norway': { 'capital':'oslo', 'population':5.084 } }
+
+# Print out the capital of France
+print(europe['france']['capital'])
+
+# Create sub-dictionary data
+data = { 'capital':'rome', 'population':59.83 }
+
+# Add data to europe under key 'italy'
+europe['italy'] = data
+print(europe)
