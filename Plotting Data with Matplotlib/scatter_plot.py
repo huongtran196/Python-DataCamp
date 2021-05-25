707a7f57ff85f47b51a8a3617ee919f40a4097b0
Huong Tran <huong.tran0196@gmail.com>
2021-05-25 14:08:26 +0300

    Adding tick, text, grid, color

--- Plotting Data with Matplotlib/scatter_plot.py
+++ Plotting Data with Matplotlib/scatter_plot.py
@@ -14,3 +14,38 @@ plt.show()
 
 plt.scatter(pop, life_exp)
 plt.show()
+
+import numpy as np
+np_pop = np.array(pop)
+np_pop = np_pop * 2
+
+plt.scatter(gdp_cap, life_exp, s = np_pop, c = col, alpha = 0.8)
+plt.xscale('log') 
+
+# Strings
+xlab = 'GDP per Capita [in USD]'
+ylab = 'Life Expectancy [in years]'
+title = 'World Development in 2007'
+
+# Add axis labels
+plt.xlabel('GDP per Capita [in USD]')
+plt.ylabel('Life Expectancy [in years]')
+
+# Add title
+plt.title('World Development in 2007')
+
+# Definition of tick_val and tick_lab
+tick_val = [1000, 10000, 100000]
+tick_lab = ['1k', '10k', '100k']
+
+# Adapt the ticks on the x-axis
+plt.xticks([1000, 10000, 100000], ['1k', '10k', '100k'])
+
+plt.text(1550, 71, 'India')
+plt.text(5700, 80, 'China')
+
+# Add grid() call
+plt.grid(True)
+
+# After customizing, display the plot
+plt.show()
