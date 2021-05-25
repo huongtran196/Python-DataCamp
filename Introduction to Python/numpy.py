e6b23dd6a5d10adc5dc5b40228e0029e2698bbbc
Huong Tran <huong.tran0196@gmail.com>
2021-05-25 12:15:53 +0300

    2D Arithmetic

--- Introduction to Python/numpy.py
+++ Introduction to Python/numpy.py
@@ -74,3 +74,20 @@ np_baseball = np.array(baseball)
 print(type(np_baseball))
 print(np_baseball.shape)
 
+# Print out the 50th row of np_baseball
+print(np_baseball[49, :])
+
+# Select the entire second column of np_baseball: np_weight_lb
+np_weight_lb = np_baseball[:, 1]
+
+# Print out height of 124th player
+print(np_baseball[123, 0])
+
+# Print out addition of np_baseball and updated
+print(np_baseball + updated)
+
+# Create numpy array: conversion
+conversion = np.array([0.0254, 0.453592, 1])
+
+# Print out product of np_baseball and conversion
+print(np_baseball * conversion)
