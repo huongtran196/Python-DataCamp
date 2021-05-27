6d18648cc1d2eee806ea640413e423f31d2caaef
Huong Tran <huong.tran0196@gmail.com>
2021-05-27 16:20:39 +0300

    Comparison Operator

---
+++ Introduction to Python/logic_and_filtering.py
@@ -0,0 +1,21 @@
+# Comparison Operator
+print(True == False)
+print(-5 * 15 != 75)
+print('pyscript' == 'PyScript')
+print(True == 1)
+x = -3 * 6
+print(x >= -10)
+y = "test"
+print('test' <= y)
+print(True > False)
+
+# Create arrays
+import numpy as np
+my_house = np.array([18.0, 20.0, 10.75, 9.50])
+your_house = np.array([14.0, 24.0, 14.25, 9.0])
+
+# my_house greater than or equal to 18
+print(my_house >= 18)
+
+# my_house less than your_house
+print(my_house < your_house)
