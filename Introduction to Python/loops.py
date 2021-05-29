b34880a960ec02a7040eb814a2778d975c50c8b1
Huong Tran <huong.tran0196@gmail.com>
2021-05-29 14:45:50 +0300

    Loop over Numpy array

--- Introduction to Python/loops.py
+++ Introduction to Python/loops.py
@@ -52,3 +52,14 @@ europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin',
 # Iterate over europe
 for key, value in europe.items() : 
     print('the capital of ' + str(key) + ' is ' + str(value))
+
+# Import numpy as np
+import numpy as np
+
+# For loop over np_height
+for x in np.nditer(np_height) : 
+    print(str(x) + ' inches')
+
+# For loop over np_baseball
+for y in np.nditer(np_baseball) : 
+    print(y)
