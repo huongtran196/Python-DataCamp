ce07575821c87b98f2ec4a11635f192810e4e51f
Huong Tran <huong.tran0196@gmail.com>
2021-06-01 22:49:49 +0300

    Define a function

---
+++ Introduction to Python/function.py
@@ -0,0 +1,8 @@
+def shout():
+    """Print a string with three exclamation marks"""
+    # Concatenate the strings: shout_word
+    shout_word = 'congratulations' + '!!!'
+    print(shout_word)
+
+shout()
+
