6b5f02cba72de9d71984beb22d46efac01025aa9
Huong Tran <huong.tran0196@gmail.com>
2021-06-01 22:56:34 +0300

    Write function with multiple parameters

--- Introduction to Python/function.py
+++ Introduction to Python/function.py
@@ -28,3 +28,16 @@ def shout(word):
 yell = shout('congratulations')
 
 print(yell)
+
+# Write functions with multiple parameters
+def shout(word1, word2):
+    """Concatenate strings with three exclamation marks"""
+    shout1 = word1 + '!!!'
+    shout2 = word2 + '!!!'
+    new_shout = shout1 + shout2
+
+    return new_shout
+
+yell = shout('congratulations', 'you')
+
+print(yell)
