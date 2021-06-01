d2cbab13be7b081a5c11d39c8947201367be7a59
Huong Tran <huong.tran0196@gmail.com>
2021-06-01 22:57:48 +0300

    Construct and Unpack tuples

--- Introduction to Python/function.py
+++ Introduction to Python/function.py
@@ -41,3 +41,12 @@ def shout(word1, word2):
 yell = shout('congratulations', 'you')
 
 print(yell)
+
+# Introduction about tuples
+# Unpack nums into num1, num2, and num3
+num1, num2, num3 = nums
+print(nums)
+
+# Construct even_nums
+even_nums = (2, 4, 6)
+print(even_nums)
