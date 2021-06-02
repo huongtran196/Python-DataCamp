476eb28dd3e6e9fe8a3746e657bb2910bc7929eb
Huong Tran <huong.tran0196@gmail.com>
2021-06-02 21:52:14 +0300

    Print dataframe object

--- Data Manipulation with Pandas/transforming_data.py
+++ Data Manipulation with Pandas/transforming_data.py
@@ -4,3 +4,7 @@ print(homelessness.info())
 print(homelessness.shape)
 print(homelessness.describe())
 
+# Part of DataFrame
+print(homelessness.value)
+print(homelessness.column)
+print(homelessness.index)
