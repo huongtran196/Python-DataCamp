894c4249d6dd500fff873fc55cb20e92e2c7e0af
Huong Tran <huong.tran0196@gmail.com>
2021-06-02 22:12:03 +0300

    Subsetting columns

--- Data Manipulation with Pandas/transforming_data.py
+++ Data Manipulation with Pandas/transforming_data.py
@@ -22,3 +22,15 @@ print(homelessness_fam.head())
 homelessness_reg_fam = homelessness.sort_values(['region', 'family_members'], ascending = [True, False])
 print(homelessness_reg_fam.head())
 
+# Create dataframe individuals
+individuals = homelessness[['individuals']]
+print(individuals.head())
+
+# Create dataframe contains state and family_members
+state_fam = homelessness[['state', 'family_members']]
+print(state_fam.head())
+
+# Create dataframe contains individuals and state
+ind_state = homelessness[['individuals', 'state']]
+print(ind_state.head())
+
