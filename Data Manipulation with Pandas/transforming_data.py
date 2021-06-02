5c0d154a8198dab357828c05cdf670d2928b5262
Huong Tran <huong.tran0196@gmail.com>
2021-06-02 22:07:30 +0300

    Sorted by columns

--- Data Manipulation with Pandas/transforming_data.py
+++ Data Manipulation with Pandas/transforming_data.py
@@ -1,3 +1,6 @@
+import pandas as pd
+homelessness = pd.read_csv('homelessness.csv')
+
 # Inspecting DataFrame
 print(homelessness.head())
 print(homelessness.info())
@@ -8,3 +11,14 @@ print(homelessness.describe())
 print(homelessness.value)
 print(homelessness.column)
 print(homelessness.index)
+
+# Sorting & subsetting
+homelessness_ind = homelessness.sort_values('individual', ascending = True)
+print(homelessness_ind.head()) #sorting column
+
+homelessness_fam = homelessness.sort_values('family_members', ascending = False)
+print(homelessness_fam.head())
+
+homelessness_reg_fam = homelessness.sort_values(['region', 'family_members'], ascending = [True, False])
+print(homelessness_reg_fam.head())
+
