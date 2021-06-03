558e93b0781ca24bc1279da9e7c88db8aa9c03cc
Huong Tran <huong.tran0196@gmail.com>
2021-06-03 23:14:03 +0300

    Calculate percentage in each store type

--- Data Manipulation with Pandas/aggregating_data.py
+++ Data Manipulation with Pandas/aggregating_data.py
@@ -55,3 +55,15 @@ print(dept_counts_sorted)
 # Get the proportion of departments of each number and sort
 dept_props_sorted = store_depts['department'].value_counts(sort= True, normalize= True)
 print(dept_props_sorted)
+
+# What percent of sales occurred at each store type?
+sales_all = sales["weekly_sales"].sum()
+
+# Subset for type A, B, C stores, calc total weekly sales
+sales_A = sales[sales["type"] == "A"]["weekly_sales"].sum()
+sales_B = sales[sales['type'] == 'B']['weekly_sales'].sum()
+sales_C = sales[sales['type'] == 'C']['weekly_sales'].sum()
+
+# Get proportion for each type
+sales_propn_by_type = [sales_A, sales_B, sales_C] / sales_all
+print(sales_propn_by_type)
