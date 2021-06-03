26107ebff6e8fccfc352196f822bc412efa5b034
Huong Tran <huong.tran0196@gmail.com>
2021-06-03 23:17:51 +0300

    Multiple grouped summaries

--- Data Manipulation with Pandas/aggregating_data.py
+++ Data Manipulation with Pandas/aggregating_data.py
@@ -80,3 +80,16 @@ print(sales_propn_by_type)
 # Group by type and is_holiday; calc total weekly sales
 sales_by_type_is_holiday = sales.groupby(['type', 'is_holiday'])['weekly_sales'].sum()
 print(sales_by_type_is_holiday)
+
+# Multiple grouped summaries
+# For each store type, aggregate weekly_sales: get min, max, mean, and median
+sales_stats = sales.groupby('type')['weekly_sales'].agg([np.min, np.max, np.mean, np.median])
+
+# Print sales_stats
+print(sales_stats)
+
+# For each store type, aggregate unemployment and fuel_price_usd_per_l: get min, max, mean, and median
+unemp_fuel_stats = sales.groupby('type')[['unemployment', 'fuel_price_usd_per_l']].agg([np.min, np.max, np.mean, np.median])
+
+# Print unemp_fuel_stats
+print(unemp_fuel_stats)
