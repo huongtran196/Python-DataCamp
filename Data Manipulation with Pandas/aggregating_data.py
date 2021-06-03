be872d11b3deb871fe0097f6f612a1816c89387e
Huong Tran <huong.tran0196@gmail.com>
2021-06-03 23:16:02 +0300

    Group by type

--- Data Manipulation with Pandas/aggregating_data.py
+++ Data Manipulation with Pandas/aggregating_data.py
@@ -67,3 +67,16 @@ sales_C = sales[sales['type'] == 'C']['weekly_sales'].sum()
 # Get proportion for each type
 sales_propn_by_type = [sales_A, sales_B, sales_C] / sales_all
 print(sales_propn_by_type)
+
+# Calculation with .groupby()
+# Group by type; calc total weekly sales
+sales_by_type = sales.groupby("type")["weekly_sales"].sum()
+print(sales_by_type)
+
+# Get proportion for each type
+sales_propn_by_type = sales_by_type / sum(sales_by_type)
+print(sales_propn_by_type)
+
+# Group by type and is_holiday; calc total weekly sales
+sales_by_type_is_holiday = sales.groupby(['type', 'is_holiday'])['weekly_sales'].sum()
+print(sales_by_type_is_holiday)
