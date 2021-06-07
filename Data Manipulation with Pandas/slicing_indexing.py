0d139226c1a7ed97a7fd64a2cefe22c3260f0ba4
Huong Tran <huong.tran0196@gmail.com>
2021-06-07 23:27:51 +0300

    Slice time series

--- Data Manipulation with Pandas/slicing_indexing.py
+++ Data Manipulation with Pandas/slicing_indexing.py
@@ -61,3 +61,16 @@ print(temperatures_srt.loc[:, 'date' : 'avg_temp_c'])
 # Subset in both directions at once
 print(temperatures_srt.loc[('India', 'Hyderabad') : ('Iraq', 'Baghdad'), 'date' : 'avg_temp_c'])
 
+# Use Boolean conditions to subset temperatures for rows in 2010 and 2011
+temperatures_bool = temperatures[(temperatures['date'] >= '2010-01-01') & (temperatures['date'] <= '2011-12-31')]
+print(temperatures_bool)
+
+# Set date as an index and sort the index
+temperatures_ind = temperatures.set_index('date').sort_index()
+
+# Use .loc[] to subset temperatures_ind for rows in 2010 and 2011
+print(temperatures_ind.loc['2010':'2011'])
+
+# Use .loc[] to subset temperatures_ind for rows from Aug 2010 to Feb 2011
+print(temperatures_ind.loc['2010-08':'2011-02'])
+
