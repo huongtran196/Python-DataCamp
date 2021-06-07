c43b5ed3ddfb01512d20ed78cd5c2316534461ef
Huong Tran <huong.tran0196@gmail.com>
2021-06-07 23:22:47 +0300

    Set multi-level indexes

--- Data Manipulation with Pandas/slicing_indexing.py
+++ Data Manipulation with Pandas/slicing_indexing.py
@@ -21,3 +21,12 @@ print(temperatures[temperatures['city'].isin(cities)])
 
 # Subset temperatures_ind using .loc[]
 print(temperatures_ind.loc[cities])
+
+# Index temperatures by country & city
+temperatures_ind = temperatures.set_index(['country', 'city'])
+
+# List of tuples: Brazil, Rio De Janeiro & Pakistan, Lahore
+rows_to_keep = [('Brazil', 'Rio De Janeiro'), ('Pakistan', 'Lahore')]
+
+# Subset for rows to keep
+print(temperatures_ind.loc[rows_to_keep])
