d9a9cf6e83165f939932dd894288a1f1c1f4366d
Huong Tran <huong.tran0196@gmail.com>
2021-06-07 23:23:34 +0300

    Sort by index

--- Data Manipulation with Pandas/slicing_indexing.py
+++ Data Manipulation with Pandas/slicing_indexing.py
@@ -30,3 +30,12 @@ rows_to_keep = [('Brazil', 'Rio De Janeiro'), ('Pakistan', 'Lahore')]
 
 # Subset for rows to keep
 print(temperatures_ind.loc[rows_to_keep])
+
+# Sort temperatures_ind by index values
+print(temperatures_ind.sort_index())
+
+# Sort temperatures_ind by index values at the city level
+print(temperatures_ind.sort_index(level = 'city', ascending = True))
+
+# Sort temperatures_ind by country then descending city
+print(temperatures_ind.sort_index(level = ['country', 'city'], ascending = [True, False]))
