adb94eddce001d70b768345c4fb8933aa6bc97c5
Huong Tran <huong.tran0196@gmail.com>
2021-06-07 23:26:55 +0300

    Slice in both directions

--- Data Manipulation with Pandas/slicing_indexing.py
+++ Data Manipulation with Pandas/slicing_indexing.py
@@ -52,3 +52,12 @@ print(temperatures_srt.loc['Lahore' : 'Moscow'])
 # Subset rows from Pakistan, Lahore to Russia, Moscow
 print(temperatures_srt.loc[('Pakistan', 'Lahore') : ('Russia', 'Moscow')])
 
+# Subset rows from India, Hyderabad to Iraq, Baghdad
+print(temperatures_srt.loc[('India', 'Hyderabad') : ('Iraq', 'Baghdad')])
+
+# Subset columns from date to avg_temp_c
+print(temperatures_srt.loc[:, 'date' : 'avg_temp_c'])
+
+# Subset in both directions at once
+print(temperatures_srt.loc[('India', 'Hyderabad') : ('Iraq', 'Baghdad'), 'date' : 'avg_temp_c'])
+
