e00a320340a4a7ae7bbbbabef22da27deb85dfc7
Huong Tran <huong.tran0196@gmail.com>
2021-06-08 22:23:15 +0300

    DataFrame to CSV

--- Data Manipulation with Pandas/create_dataframe.py
+++ Data Manipulation with Pandas/create_dataframe.py
@@ -35,3 +35,10 @@ airline_totals = airline_bumping.groupby('airline')[['nb_bumped', 'total_passeng
 airline_totals["bumps_per_10k"] = airline_totals['nb_bumped'] / airline_totals['total_passengers'] * 10000
 print(airline_totals)
 
+# Create airline_totals_sorted
+airline_totals_sorted = airline_totals.sort_values('bumps_per_10k', ascending = False)
+print(airline_totals_sorted)
+
+# Save as airline_totals_sorted.csv
+airline_totals_sorted.to_csv('airline_totals_sorted.csv')
+
