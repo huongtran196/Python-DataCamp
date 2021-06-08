cf71ed5b0ecb8d7934a072ce683f33ce16357ab3
Huong Tran <huong.tran0196@gmail.com>
2021-06-08 22:13:24 +0300

    Find missing value

---
+++ Data Manipulation with Pandas/missing_value.py
@@ -0,0 +1,17 @@
+# Finding missing value
+import pandas as pd
+import matplotlib.pyplot as plt
+
+avocados = pd.read_csv('avocados_sales.csv')
+avocados_2016 = avocados[avocados['year'] == '2016']
+# Check individual values for missing values
+print(avocados_2016.isna())
+
+# Check each column for missing values
+print(avocados_2016.isna().any())
+
+# Bar plot of missing values by variable
+avocados_2016.isna().sum().plot(kind='bar')
+
+# Show plot
+plt.show()
