28b58d319cf9f1345a630106ca6e4e398cc58f8e
Huong Tran <huong.tran0196@gmail.com>
2021-06-14 23:29:05 +0300

    Filtered df and aggregate col

--- Joining Data with Pandas/merge_multi_df_1.py
+++ Joining Data with Pandas/merge_multi_df_1.py
@@ -10,3 +10,11 @@ print(ridership_cal)
 ridership_cal_stations = ridership.merge(cal, on=['year', 'month', 'day']) \
                                 .merge(stations, on='station_id')
 print(ridership_cal_stations)
+
+# Create filter criteria
+filter_criteria = (ridership_cal_stations['station_name'] == 'Wilson')
+                    & (ridership_cal_stations['day_type'] == 'Weekday')
+                    & (ridership_cal_stations['month'] == 7)
+
+# Filter rows with the previous condition and sum the ride column
+print(ridership_cal_stations.loc[filter_criteria, 'ride'].sum())
