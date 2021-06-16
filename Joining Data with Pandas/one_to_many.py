6b8e78c249e76d077eaf2c0dbbb68c5b547e8f0c
Huong Tran <huong.tran0196@gmail.com>
2021-06-16 22:21:29 +0300

    Join tables and aggregate data

--- Joining Data with Pandas/one_to_many.py
+++ Joining Data with Pandas/one_to_many.py
@@ -2,8 +2,14 @@
 import pandas as pd
 licenses = pd.read_csv('licenses.csv')
 biz_owners = pd.read_csv('biz_owners.csv')
+wards = pd.read_csv('wards.csv')
+zip_demo = pd.read_csv('zip_demo.csv')
 
 licenses_owners = licenses.merge(biz_owners, on='account')
 counted_df = licenses_owners.groupby('title').agg({'account':'count'})
 sorted_df = counted_df.sort_values('account', ascending=False)
 print(sorted_df.head())
+
+licenses_zip_ward = licenses.merge(zip, on='zip') \
+                        .merge(wards, on='ward')
+print(licenses_zip_ward.groupby('alderman').agg({'income':'median'}))
